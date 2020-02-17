62de9d24f433047eabf0d7bc109d1e5efa75cf02
Felix Timmel <f.timmel95@gmail.com>
2020-02-17 12:28:17 -0500

    Stupid coach ask

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/ask', to: 'questions_controller#ask'
-  get '/answer', to: 'questions_controller#answer'
+  get '/ask', to: 'questions_controller#ask', as: :ask
+  get '/answer', to: 'questions_controller#answer', as: :answer
   get 'pages/ask'
   get 'pages/answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
